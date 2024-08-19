d344087dbbb46ddb4126fc2d586c38ad92505308
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-08-19 12:19:48 +0500

    installed and configured: animation on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+  useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+  });
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
